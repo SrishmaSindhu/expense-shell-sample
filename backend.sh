1380caca49e786c7f518473a39afea65d324a275
Sindhuja Addala <sindhu.a@saince.in>
2024-04-23 20:24:11 +0530

    mysql

--- backend.sh
+++ backend.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%s)
-SCRIPTNAME=$( echo $0 | cut -d "." -f1 )
-LOGFILE=$TIMESTAMP-$SCRIPTNAME.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
 echo "please enter db password"
 read -s Mysql_root_password
-
-
-if [ $USERID -ne 0 ]
-then 
-    echo "please run the script with root access"
-    exit 1
-else     
-    echo  "your super user"
-fi
-
-VALIDATE() {
-    if [ $1 -ne 0 ]
-     then 
-        echo -e "$2 ..$R FAILED $N"
-     else 
-           
-       echo -e " $2... $G SUCCESS $N"
-  fi  
-}
 
   dnf module disable nodejs -y &>>$LOGFILE
   VALIDATE $? "disabling nodejs"
